2752736a7b33470a861a3c121a9d990fa8552359
Kaleem Ashraf <kaleemashraf677@gmail.com>
2023-11-10 08:50:56 +0530

    Initial Commit

---
+++ DesignPattern/src/com/designpattern/Square.java
@@ -0,0 +1,11 @@
+package com.designpattern;
+
+public class Square implements Shape {
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+	      System.out.println("Inside Square::draw() method.");
+	}
+
+}
